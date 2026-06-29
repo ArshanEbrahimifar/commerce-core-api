de93a4c9669a3c6f4323d54ff961b54d40e89278
Arshan Ebrahimifar <227443762+ArshanEbrahimifar@users.noreply.github.com>
2026-06-29 22:07:27 +0300

    refactor: organize API routes

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,13 @@
-import express, { type Request, type Response } from "express";
+import express from "express";
 import { errorHandlerMiddleware } from "./middlewares/error-handler.middleware";
 import { notFoundMiddleware } from "./middlewares/not-found.middleware";
+import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/health", (_req: Request, res: Response) => {
-  res.status(200).json({
-    success: true,
-    status: "ok",
-    message: "Commerce Core API is running",
-  });
-});
+app.use("/api/v1", routes);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
