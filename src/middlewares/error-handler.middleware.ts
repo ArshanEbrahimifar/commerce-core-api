dbdc806bba457087cd19e9ba64439ac513916534
Arshan Ebrahimifar <227443762+ArshanEbrahimifar@users.noreply.github.com>
2026-06-30 19:54:37 +0300

    feat: add async error handling utilities

--- src/middlewares/error-handler.middleware.ts
+++ src/middlewares/error-handler.middleware.ts
@@ -1,5 +1,6 @@
-import { NextFunction, Request, Response } from "express";
+import { type NextFunction, type Request, type Response } from "express";
 import { env } from "../config/env";
+import { AppError } from "../utils/app-error";
 
 export const errorHandlerMiddleware = (
   error: Error,
@@ -7,7 +8,13 @@ export const errorHandlerMiddleware = (
   res: Response,
   _next: NextFunction,
 ) => {
-  res.status(500).json({
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      success: false,
+      message: error.message,
+    });
+  }
+  return res.status(500).json({
     success: false,
     message: "Internal server error",
     error: env.NODE_ENV === "development" ? error.message : undefined,
