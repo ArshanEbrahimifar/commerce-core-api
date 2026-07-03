1e2163e823c1c182c6c518cb7159722fb4137570
Arshan Ebrahimifar <227443762+ArshanEbrahimifar@users.noreply.github.com>
2026-07-03 15:08:45 +0300

    feat: add product creation service

---
+++ src/modules/product/product.service.ts
@@ -0,0 +1,17 @@
+import { Product } from "./product.model";
+import { type CreateProductInput } from "./product.shema";
+
+export const createProduct = async (
+  input: CreateProductInput,
+  userId: string,
+) => {
+  const product = await Product.create({
+    name: input.name,
+    description: input.description,
+    price: input.price,
+    stock: input.stock,
+    category: input.category,
+    createdBy: userId,
+  });
+  return product;
+};
