4e857c25c8a0ddc7e03bf8dc212eae210afd268a
Arshan Ebrahimifar <227443762+ArshanEbrahimifar@users.noreply.github.com>
2026-07-09 17:58:33 +0300

    feat: add cart item update controller

--- src/modules/cart/cart.controller.ts
+++ src/modules/cart/cart.controller.ts
@@ -1,7 +1,15 @@
 import { AuthenticatedRequest } from "../../middlewares/auth.middleware";
 import { asyncHandler } from "../../utils/async-handler";
-import { AddCartItemInput } from "./cart.schema";
-import { addItemToCart, getCartByUserId } from "./cart.service";
+import {
+  AddCartItemInput,
+  CartItemParams,
+  UpdateCartItemInput,
+} from "./cart.schema";
+import {
+  addItemToCart,
+  getCartByUserId,
+  updateCartItemQuantity,
+} from "./cart.service";
 
 export const addCartItemController = asyncHandler(async (req, res) => {
   const { userId } = req as AuthenticatedRequest;
@@ -25,6 +33,21 @@ export const getCartController = asyncHandler(async (req, res) => {
   res.status(200).json({
     success: true,
     message: "Cart fetched successfully",
+    data: {
+      cart,
+    },
+  });
+});
+export const updateCartItemController = asyncHandler(async (req, res) => {
+  const { userId } = req as AuthenticatedRequest;
+  const input = req.body as UpdateCartItemInput;
+  const { productId } = req.params as CartItemParams;
+
+  const cart = await updateCartItemQuantity(userId, productId, input);
+
+  res.status(200).json({
+    success: true,
+    message: "Cart item updated successfully",
     data: {
       cart,
     },
