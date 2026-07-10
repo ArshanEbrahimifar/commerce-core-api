c469b5fdb2d0e8d1125d8c473ce254ea0a9a2030
Arshan Ebrahimifar <227443762+ArshanEbrahimifar@users.noreply.github.com>
2026-07-10 21:35:08 +0300

    feat: add order creation service

---
+++ src/modules/order/order.service.ts
@@ -0,0 +1,42 @@
+import { AppError } from "../../utils/app-error";
+import { Cart } from "../cart/cart.model";
+import { Order } from "./order.model";
+
+export const createOrderFromCart = async (userId: string) => {
+  const cart = await Cart.findOne({ user: userId }).populate("items.product");
+
+  if (!cart || cart.items.length === 0) {
+    throw new AppError("Cart is empty", 400);
+  }
+  const orderItems = cart.items.map((item) => {
+    const product = item.product as any;
+
+    if (!product || !product._id) {
+      throw new AppError("Product not found", 404);
+    }
+
+    return {
+      product: product._id,
+      name: product.name,
+      price: product.price,
+      quantity: item.quantity,
+    };
+  });
+
+  const totalAmount = orderItems.reduce((total, item) => {
+    return total + item.price * item.quantity;
+  }, 0);
+
+  const order = await Order.create({
+    user: userId,
+    items: orderItems,
+    totalAmount,
+    status: "pending",
+  });
+
+  cart.items = [];
+
+  await cart.save();
+
+  return order;
+};
